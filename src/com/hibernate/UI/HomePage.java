41386932d9817bba4f02b5310822c466d65cde14
Sriramakash Palanivelu <sriramakash222@gmail.com>
2022-09-20 16:37:52 +0530

    CRUD operation Successfully Completed

--- src/com/hibernate/UI/HomePage.java
+++ src/com/hibernate/UI/HomePage.java
@@ -7,55 +7,70 @@ import com.hibernate.model.Employee;
 
 public class HomePage {
 
-	public static void main(String[] args) 
-	{
-		Scanner sc =new Scanner(System.in);
-		int ch;
-		String name,domain,location;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ch, id;
+		String name, domain, location,updatevalue;
 		long salary;
 		CRUD_OperationDAO operation = new CRUD_OperationDAO();
-		
+
 		System.out.println("------Welcome to Hibernate Project------");
 		System.out.println("-----We can Perform a CRUD operation------");
-		
-		do
-		{
-		System.out.println("1.Insert Employee record");
-		System.out.println("2.View Employee record");
-		System.out.println("3.View all the Employee record");
-		System.out.println("4.Update Employee record");
-		System.out.println("5.Delete Employee record");
-		System.out.println("0.Exit");
-		ch=sc.nextInt();
-		switch(ch)
-		{
-		case 1:	System.out.println("----- Insert record -----");
+
+		do {
+			System.out.println("1.Insert Employee record");
+			System.out.println("2.View Employee record");
+			System.out.println("3.View all the Employee record");
+			System.out.println("4.Update Employee record");
+			System.out.println("5.Delete Employee record");
+			System.out.println("0.Exit");
+			ch = sc.nextInt();
+			switch (ch) {
+			case 1:
+				System.out.println("----- Insert record -----");
 				System.out.println("Enter the name");
-				name=sc.next();
+				name = sc.next();
 				System.out.println("Enter the domain");
-				domain=sc.next();
+				domain = sc.next();
 				System.out.println("Enter the location");
-				location=sc.next();
+				location = sc.next();
 				System.out.println("Enter the salary");
-				salary=sc.nextLong();
-				Employee emp=new Employee(name,domain,location,salary);
+				salary = sc.nextLong();
+				Employee emp = new Employee(name, domain, location, salary);
 				operation.insertRecord(emp);
-			
-		
 				break;
-		case 2:System.out.println("insert");
+			case 2:
+				System.out.println("----- View record -----");
+				System.out.println("Enter the Id");
+				id = sc.nextInt();
+				operation.viewRecord(id);
 				break;
-		case 3:System.out.println("insert");
+			case 3:
+				System.out.println("----- View All record -----");
+				operation.viewAllRecord();
 				break;
-		case 4:System.out.println("insert");
+			case 4:
+				System.out.println("----- Update record -----");
+				System.out.println("Enter name for update :");
+				name=sc.next();
+				System.out.println("Enter the Id");
+				id = sc.nextInt();
+				operation.updateRecord(name,id);
 				break;
-		case 5:System.out.println("insert");
+			case 5:
+				System.out.println("----- Delete record -----");
+				System.out.println("Enter the Id");
+				id = sc.nextInt();
+				operation.deleteRecord(id);
 				break;
-		case 0:System.out.println("insert");
+			case 0:
+				System.out.println("Thank You");
+				operation.closeall();
 				break;
-		default:System.out.println("Invalid Choice");
-		}
-		}while(ch!=0);
+			default:
+				System.out.println("Invalid Choice");
+			}
+		} while (ch != 0);
 
 	}
 
--- src/com/hibernate/dao/CRUD_OperationDAO.java
+++ src/com/hibernate/dao/CRUD_OperationDAO.java
@@ -11,48 +11,59 @@ import com.hibernate.model.Employee;
 
 public class CRUD_OperationDAO {
 	SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-	Session session=null;
-	Transaction tx=null;
-	
-	public void insertRecord(Employee e1)
-	{
-			session =factory.getCurrentSession();
-			tx=session.beginTransaction();
-			session.save(e1);
-			tx.commit();
+	Session session = null;
+	Transaction tx = null;
+
+	public void insertRecord(Employee e1) {
+		session = factory.getCurrentSession();
+		tx = session.beginTransaction();
+		session.save(e1);
+		System.out.println("Insert Record Successfully");
+		tx.commit();
 	}
-	public void deleteRecord()
-	{
-			session =factory.getCurrentSession();
-			tx=session.beginTransaction();
-			session.createQuery("delete from employee where id = "+1).executeUpdate();
-			tx.commit();
+
+	public void deleteRecord(int id) {
+		session = factory.getCurrentSession();
+		tx = session.beginTransaction();
+		session.createQuery("delete from Employee where id = " + id).executeUpdate();
+		System.out.println("Delete Record Successfully");
+		tx.commit();
 	}
-	public void updateRecord()
-	{
-			session =factory.getCurrentSession();
-			tx=session.beginTransaction();
-			//session.createQuery("update employee set"+ +"where id = "+1).executeUpdate();
-			tx.commit();
+
+	public void updateRecord(String name,int id) {
+		session = factory.getCurrentSession();
+		tx = session.beginTransaction();
+		session.createQuery("update Employee set name = '"+name+"' where id = "+id).executeUpdate();
+		System.out.println("update Record Successfully");
+		tx.commit();
 	}
-	public void ViewRecord()
-	{
-			session =factory.getCurrentSession();
-			tx=session.beginTransaction();
-			Employee employee=session.get(Employee.class, 1);
+
+	public void viewRecord(int id) {
+		try {
+			session = factory.getCurrentSession();
+			tx = session.beginTransaction();
+			Employee employee = session.get(Employee.class, id);
 			System.out.println(employee);
+			
 			tx.commit();
+		} catch (Exception e) {
+			System.out.println("Invalid ID ");
+		}
 	}
-	public void viewAllRecord()
+
+	public void viewAllRecord() {
+		session = factory.getCurrentSession();
+		tx = session.beginTransaction();
+		List<Employee> employees = session.createQuery("from Employee").list();
+		for (Employee emp : employees) {
+			System.out.println(emp);
+		}
+		tx.commit();
+	}
+
+	public void closeall()
 	{
-			session =factory.getCurrentSession();
-			tx=session.beginTransaction();
-			List<Employee> employee=session.createQuery("from employee").list();
-			for(Employee emp:employee)
-			{
-				System.out.println(emp);
-			}
-			tx.commit();
+		session.close();
+		factory.close();
 	}
-	
 }
